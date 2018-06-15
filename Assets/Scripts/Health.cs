ca0e389f835c8c0a616f53ce4763e0eaf7a2c32a
runemyth0 <runemyth0@gmail.com>
2018-06-14 21:44:34 -0500

    HP/Damage System v1

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	public float healthPoints;
+	public float damagePoints;
+
+	private float currentHealthPoints;
+
+	void Start ()
+	{
+		currentHealthPoints = healthPoints;
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Attack")
+		{
+			currentHealthPoints -= damagePoints;
+			if (currentHealthPoints <= 0.0f)
+			{
+				Destroy (gameObject);
+			}
+		}
+	}
+}
