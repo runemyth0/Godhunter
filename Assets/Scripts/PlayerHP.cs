a248ce884a80449a3806fd252e935470b7b7cef7
runemyth0 <runemyth0@gmail.com>
2018-06-29 10:36:01 -0500

    Added player HP value display and other tweaks

---
+++ Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour
+{
+	public Text hpText;
+
+	private Health health;
+
+	void Awake ()
+	{
+		// Get player's health script, in order to access hp values
+		health = GetComponent<Health>();
+	}
+
+	void Update ()
+	{
+		// Set text
+		hpText.text = health.currentHP + "/" + health.maxHP;
+	}
+}
